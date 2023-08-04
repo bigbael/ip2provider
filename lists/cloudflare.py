d45db507a68501186115ba3971b23e54100a9a88
bigbael <bigbael@users.noreply.github.com>
2023-08-03 19:43:45 -0500

    Update cloudflare.py

--- lists/cloudflare.py
+++ lists/cloudflare.py
@@ -8,17 +8,20 @@ from bs4 import BeautifulSoup as bs
 def update():
 	results = []
 
-	response = requests.get('https://www.cloudflare.com/en-ca/ips/')
+	#response = requests.get('https://www.cloudflare.com/en-ca/ips/')
+    response = requests.get('https://www.cloudflare.com/ips-v4')
 
 	if response.status_code != 200:
 		return False
 
 	soup = bs(response.text, features='html.parser')
-	elements = soup.find('h5',string='IPv4').parent.find_all('li')
-
-	for elem in elements:
-		cidr = elem.text
-		results.append("%s %s unknown unknown" % (cidr, 'cloudflare'))
+	#elements = soup.find('h5',string='IPv4').parent.find_all('li')
+    elements = soup.text
+    
+	for elem in elements.split("\n"):
+		#cidr = elem.text
+		cidr = elem
+        results.append("%s %s unknown unknown" % (cidr, 'cloudflare'))
 
 	# Write results to file
 	with open('data/cloudflare.txt', 'w') as f:
